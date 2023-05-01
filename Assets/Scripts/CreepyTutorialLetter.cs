07b98bc01de1b27705d0cc0dc292c31e9a676dc0
lyrari <lyrariley1@gmail.com>
2023-05-01 02:25:34 -0700

    Gameover

--- Assets/Scripts/CreepyTutorialLetter.cs
+++ Assets/Scripts/CreepyTutorialLetter.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class CreepyTutorialLetter : MonoBehaviour
 {
     bool eventTriggered;
+    BoolSO tutorialMode;
+
     // On letter pickup:
     // Cutscene plays (Fade to red slowly)
     // Creepy letter sound plays ("demonNote"), screen goes full black (fade in after 5 secs?)
@@ -12,6 +14,7 @@ public class CreepyTutorialLetter : MonoBehaviour
     // Timer starts and appears (5:00 counting down)
     // Mail timer has to start 
     // SpookMeter begins rising 
+    // Reset failures to 0
     // Light turning off timer starts
     // Screen fades in over the next couple seconds
     // Enable Demon letters spawning and demon seals (if we get to that)
@@ -22,5 +25,7 @@ public class CreepyTutorialLetter : MonoBehaviour
         eventTriggered = true;
         AkSoundEngine.PostEvent("demonNote", this.gameObject);
         Debug.Log("CREEPY MODE ACTIVATE");
+
+        tutorialMode.value = false;
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,83 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public int FailuresForDeath = 3;
+    public int SuccessesForWin = 30;
+
+    public GameObject GameOverPanel;
+    public TMP_Text GameOverMessage;
+
+    public ScoreTracker m_ScoreTracker;
+    public BoolSO m_TutorialMode;
+    public BoolSO m_GameOver;
+
+    public GameObject ScreenCoveringEffect;
+    public Light SceneLight;
+    public Color VictoryColor;
+    public Color FailureColor;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_GameOver.value = false;
+        m_TutorialMode.value = true;
+        GameOverPanel.SetActive(false);
+
+        ScreenCoveringEffect.GetComponent<Animator>().SetTrigger("FadeFromBlack");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        if (m_GameOver.value) {
+            if (Input.anyKeyDown)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+            return;
+        }
+       
+
+        if (!m_TutorialMode.value && m_ScoreTracker.failures >= FailuresForDeath)
+        {
+            Debug.Log("Game over");
+            Image screenCoverImage = ScreenCoveringEffect.GetComponent<Image>();
+            screenCoverImage.color = FailureColor;
+            screenCoverImage.GetComponent<Animator>().SetTrigger("FadeToBlack");
+            StartCoroutine(IncreaseLightIntensity(-0.1f));
+
+            m_GameOver.value = true;
+            GameOverPanel.SetActive(true);
+            GameOverMessage.text = "Delivered to the abyss...";
+        }
+
+        if (m_ScoreTracker.successes >= SuccessesForWin) {
+            Debug.Log("You win");
+            Image screenCoverImage = ScreenCoveringEffect.GetComponent<Image>();
+            screenCoverImage.GetComponent<Animator>().SetTrigger("FadeToBlack");
+            screenCoverImage.color = VictoryColor;
+            StartCoroutine(IncreaseLightIntensity(0.1f));
+
+            m_GameOver.value = true;
+            GameOverPanel.SetActive(true);
+            GameOverMessage.text = "You won!";
+        }
+    }
+
+    IEnumerator IncreaseLightIntensity(float amount)
+    {
+        SceneLight.intensity += amount;
+        yield return new WaitForSeconds(0.1f);
     }
 }
